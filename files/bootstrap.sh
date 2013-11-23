50920d83b4d6159eb7bfcfddab0278f9591e429c
Peter Pouliot <ppouliot@gmail.com>
2013-11-23 02:38:14 -0500

    trying to build a class to get a working container

--- files/bootstrap.sh
+++
@@ -1,2 +0,0 @@
-ln -s modules/ipam/files/hiera.example hiera
-ln -s ns1.yaml 00155d2a7419.yaml
--- tests/bootstrap.pp
+++ tests/bootstrap.pp
@@ -1,6 +1,13 @@
 class hieratest {
+  file {'/tmp/bootstrap.sh':
+    ensure  => file,
+    content => tempate('ipam/bootstrap.erb'),
+    owner   => root,
+    group   => root,
+    mode    => "0777",
+  }
   exec {"ipam_install_sample_hiera_and_apply_manifest":
-    command => "/etc/puppet/modules/ipam/files/bootstrap.sh",
-    cwd     => "/etc/puppet",
+    command => "/tmp/bootstrap.sh",
+    cwd     => "/tmp",
   }
 }
