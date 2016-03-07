1820927d6e2e6deb153fd41869f0f1426668630c
peter pouliot <ppouliot@microsoft.com>
2016-03-07 20:15:39 +0000

    cleanup of test files

--- tests/bootstrap.pp
+++ tests/bootstrap.pp
@@ -1,13 +1,12 @@
-class hieratest {
-  file {'/tmp/bootstrap.sh':
-    ensure  => file,
-    content => tempate('ipam/bootstrap.erb'),
-    owner   => root,
-    group   => root,
-    mode    => "0777",
-  }
-  exec {"ipam_install_sample_hiera_and_apply_manifest":
-    command => "/tmp/bootstrap.sh",
-    cwd     => "/tmp",
-  }
+# Setup Hiera and run script
+file {'/tmp/bootstrap.sh':
+  ensure  => file,
+  content => tempate('ipam/bootstrap.erb'),
+  owner   => root,
+  group   => root,
+  mode    => '0777',
+} ->
+exec {'ipam_install_sample_hiera_and_apply_manifest':
+  command => '/tmp/bootstrap.sh',
+  cwd     => '/tmp',
 }
