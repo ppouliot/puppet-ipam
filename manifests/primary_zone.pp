e2b7dc3c7c39a9915db9480c6054edb12afc30a5
Peter J. Pouliot <peter@pouliot.net>
2017-12-04 12:42:07 -0500

    updating

--- manifests/primary_zone.pp
+++ manifests/primary_zone.pp
@@ -1,10 +1,12 @@
 # == Class: ipam::primary_zone
-define ipam::primary_zone ($soa,$soa_email,$nameservers,$allow_transfer){
+define ipam::primary_zone ($soa,$soa_email,$nameservers,$allow_transfer,$allow_update,$also_notify){
   dns::zone{ $name:
     soa            => $soa,
     soa_email      => $soa_email,
     nameservers    => $nameservers,
     allow_transfer => $allow_transfer,
+    allow_update   => $allow_update,
+    also_notify    => $also_notify,
     require        => Class['ipam::config'],
   }
 }
