6631d4c7a3e3e4bf908b1aa25c2933a7643ebe51
Peter Pouliot <peter@pouliot.net>
2018-02-02 10:30:57 -0500

    updating spec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,3 +34,10 @@ end
 RSpec.configure do |c|
   c.default_facts = default_facts
 end
+
+RSpec.configure do |c|
+  c.hiera_config = \
+    File.expand_path(
+      File.join(__FILE__, '../fixtures/hiera.yaml')
+    )
+end
