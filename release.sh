83b2defc626343be4fbde841c48e4308d562ba09
Peter Pouliot <peter@pouliot.net>
2018-02-12 08:37:03 -0500

    updating release to have additionaly platforms included in push

--- release.sh
+++ release.sh
@@ -9,6 +9,7 @@ git pull
 docker run --rm -v "$PWD":/app treeder/bump patch
 version=`cat VERSION`
 echo "version: $version"
+#sed -i 's/"version\"\:/c\"version\"\:\ \"${version}/' metadata.json
 # run build
 ./build.sh
 # tag it
@@ -17,7 +18,10 @@ git commit -m "version $version"
 git tag -a "$version" -m "version $version"
 git push
 git push --tags
-docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version $USERNAME/IMAGE:$version-centos $USERNAME/$IMAGE:$version-debian $USERNAME/$IMAGE:$version $USERNAME/$IMAGE:$version-ubuntu
 # push it
 docker push $USERNAME/$IMAGE:latest
 docker push $USERNAME/$IMAGE:$version
+docker push $USERNAME/IMAGE:$version-centos
+docker push $USERNAME/$IMAGE:$version-debian 
+docker push $USERNAME/$IMAGE:$version-ubuntu
