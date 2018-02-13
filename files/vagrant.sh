7c8f3baf5a5010bd1745baffe25be29fe9fac37d
Peter Pouliot <peter@pouliot.net>
2018-02-13 17:04:22 -0500

    updating

--- files/vagrant.sh
+++ files/vagrant.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 /opt/puppetlabs/puppet/bin/gem install r10k hiera-eyaml
+cd /etc/puppetlabs/code/environments/production
+cp modules/ipam/Puppetfile /etc/puppetlabs/code/environments/production/Puppetfile
+/opt/puppetlabs/puppet/bin/r10k puppetfile install 
+/opt/puppetlabs/puppet/bin/r10k puppetfile install 
