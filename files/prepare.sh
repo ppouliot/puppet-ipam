13f632184329c7f0f4e0d69e8a3954365220b599
Peter Pouliot <peter@pouliot.net>
2018-02-14 09:27:10 -0500

    Updating

--- files/prepare.sh
+++ files/prepare.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -x
-if [ -d /usr/bin/yum ];
-then
-  yum install git
+if [ -d /usr/bin/yum ]; then
+  echo "Yum is detected installing required packages for the platform."
+  yum install git wget curl
+elif [ -d /usr/bin/apt ]; then
+  echo "Apt is detected installing required packages for the platform."
+  apt-get install git wget curl -y
+else 
+  echo "No supported Package Management"
 fi
