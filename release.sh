e7bbc6e838ec591fb7faa80e7de35ad96f4b225e
Peter J. Pouliot <peter@pouliot.net>
2018-02-16 16:25:50 -0500

    updating

--- release.sh
+++ release.sh
@@ -46,10 +46,12 @@ fi
 
 
 asciinema rec -q --title="BuildLog-$IMAGE-$VERSION-Docker" -c './build.sh -d' ./.wiki/json/buildlog-$IMAGE-$VERSION-docker.json
-docker run --rm -v $PWD:/data asciinema/asciicast2gif -s 2 -t solarized-dark ./.wiki/json/buildlog-$IMAGE-$VERSION-docker.json /.wiki/img/buildlog-$IMAGE-$VERSION-docker.gif ;
+echo "# Docker $USER/$IMAGE:$VERSION" >> BUILDLOG.gifs.sh.md
+echo "docker run --rm -v $PWD:/data asciinema/asciicast2gif -s 2 -t solarized-dark ./.wiki/json/buildlog-$IMAGE-$VERSION-docker.json /.wiki/img/buildlog-$IMAGE-$VERSION-docker.gif" >> ./.wiki/BUILDLOG.gifs.sh.md
 
 asciinema rec -q --title="BuildLog-$IMAGE-$VERSION-Vagrant" -c './build.sh -d' ./.wiki/json/buildlog-$IMAGE-$VERSION-vagrant.json
-docker run --rm -v $PWD:/data asciinema/asciicast2gif -s 2 -t solarized-dark ./.wiki/json/buildlog-$IMAGE-$VERSION-vagrant.json /.wiki/img/buildlog-$IMAGE-$VERSION-vagrant.gif ;
+echo "# Docker $USER/$IMAGE:$VERSION" >> BUILDLOG.gifs.sh.md
+echo "docker run --rm -v $PWD:/data asciinema/asciicast2gif -s 2 -t solarized-dark ./.wiki/json/buildlog-$IMAGE-$VERSION-vagrant.json /.wiki/img/buildlog-$IMAGE-$VERSION-vagrant.gif" >> ./.wiki/BUILDLOG.gifs.sh.md
 
 
 
