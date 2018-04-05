16afcb717aa7bed6cde9a5f4e2b6fcd8ed45d5ff
Peter Pouliot <peter@pouliot.net>
2018-04-05 11:23:36 -0400

    updating

--- files/create_omapi.sh
+++ files/create_omapi.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-exec &>> /tmp/create_omapi_key.log
-echo "**** Script to create omapi keys for IPAM cluster ****"
-echo "**** Determining Key Variables ****"
+exec &>> $HOME/create_omapi_key.log
+echo "**** Script to create omapi keys for IPAM cluster **********"
+echo "**** Determining Key Variables *****************************"
 
 OMAPI_KEY_NAME=`hostname -f`
 DN=`hostname -d`
@@ -16,7 +16,7 @@ do
   OMAPI_KEYS_DIR=`find / -name bind.keys.d`
   echo -n "#"
 done
-echo "****"$OMAPI_KEYS_DIR" Found ****"
+echo "**** "$OMAPI_KEYS_DIR" Found ***********************"
 cd $OMAPI_KEYS_DIR
 if [[ $OMAPI_KEY_NAME == $SECONDARY ]]; then
   rsync -av -e ssh root@$PRIMARY:${REMOTE_OMAPI_KEYS_DIR}/${OMAPI_KEYS_DIR}/
@@ -24,7 +24,7 @@ exit
 else
 echo "**** Creating OMAPI Key ****"
 dnssec-keygen -r /dev/urandom -a HMAC-MD5 -b 512 -n HOST $OMAPI_KEY_NAME
-echo "**** Creating OMAPI Secret FIle  ****"
+echo "**** Creating OMAPI Secret FIle  ***************************"
 export OMAPI_SECRET=`cat $OMAPI_KEYS_DIR/K${OMAPI_KEY_NAME}.+*.private |grep ^Key| cut -d ' ' -f2-`
 echo 'secret "'$OMAPI_SECRET'";' > ${OMAPI_KEYS_DIR}/${OMAPI_KEY_NAME}.secret
 echo "**** Creating OMAPI Key FIle  ****"
