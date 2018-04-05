b0423b109decf8dcb09aa15e5943ec2defa879b0
Peter Pouliot <peter@pouliot.net>
2018-04-05 12:00:17 -0400

    updating

--- files/prepare.sh
+++ files/prepare.sh
@@ -3,17 +3,19 @@ set -x
 YUM=`which yum`
 APT=`which apt-get`
 if [ $YUM ]; then
-  echo "Yum is detected installing required packages for the platform."
+  echo "**** Yum is detected installing required packages for the platform. ****"
   $YUM install git wget curl -y
 elif [ $APT ]; then
-  echo "Apt is detected installing required packages for the platform."
+  echo "**** Apt is detected installing required packages for the platform. ****"
   $APT install git wget curl -y
 else 
-  echo "No supported Package Management"
+  echo "**** No supported Package Management ****"
 fi
-
+echo "**** Populating /etc/hosts with Reousrce names ****"
 cat <<EOF >> /etc/hosts 
 192.168.0.2 ipam1.contoso.ltd ipam1
 192.168.0.3 ipam2.contoso.ltd ipam2
-
 EOF
+
+echo "**** creating /etc/puppetlabs/puppet/data directory  ****"
+mkdir -p /etc/puppetlabs/puppet/data
