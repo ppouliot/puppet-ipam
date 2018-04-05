2f04caddaf4845e61c498bfe73c70423362dc515
Peter Pouliot <peter@pouliot.net>
2018-04-05 14:22:50 -0400

    updating

--- files/create_omapi.sh
+++ files/create_omapi.sh
@@ -33,8 +33,8 @@ rndc-confgen -r /dev/urandom -A HMAC-MD5 -b 512 -c $OMAPI_KEY_NAME
 echo "**** Creating OMAPI Key ****"
 dnssec-keygen -r /dev/urandom -a HMAC-MD5 -b 512 -n HOST $OMAPI_KEY_NAME
 echo "**** Creating OMAPI Secret FIle  ***************************"
-# export OMAPI_PRIVATE_KEY=`cat $OMAPI_KEYS_DIR/K${OMAPI_KEY_NAME}.+*.private |grep ^Key| cut -d ' ' -f2-`
-export OMAPI_PRIVATE_KEY=`cat $OMAPI_KEYS_DIR/K${OMAPI_KEY_NAME}.+*.public |grep ^Key| cut -d ' ' -f2-`
+export OMAPI_PRIVATE_KEY=`cat $OMAPI_KEYS_DIR/K${OMAPI_KEY_NAME}.+*.private |grep ^Key| cut -d ' ' -f2-`
+# export OMAPI_PRIVATE_KEY=`cat $OMAPI_KEYS_DIR/K${OMAPI_KEY_NAME}.+*.public |grep ^Key| cut -d ' ' -f2-`
 echo 'secret "'$OMAPI_PRIVATE_KEY'";' > ${OMAPI_KEYS_DIR}/${OMAPI_KEY_NAME}.secret
 echo "**** Creating OMAPI Key FIle  ****"
 cat <<EOF > ${OMAPI_KEYS_DIR}/${OMAPI_KEY_NAME}.key
