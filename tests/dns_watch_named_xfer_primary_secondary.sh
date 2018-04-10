dcbbb8c3af046b6dd2228ca5b26dbe8fa07bac46
Peter Pouliot <peter@pouliot.net>
2018-04-10 01:10:47 -0400

    updating echo output

--- tests/dns_watch_named_xfer_primary_secondary.sh
+++ tests/dns_watch_named_xfer_primary_secondary.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 if [ -e /var/log/messages ]; then
-  echo "!!!!!!!!!! /var/log/messages detected !!!!!!!!!!"
-  echo "!!!!!!!!!!  waiting for dns named xfer !!!!!!!!!!!"
+  echo "!!!!!!!!!! Detected /var/log/messages !!!!!!!!!!!!!!!!!!!!!!!!"
+  echo "?????????? Waiting for BIND named XFER ???????????????????????"
   tail -f /var/log/messages | grep --line-buffered -Ei 'AXFR started' -m 2
-  echo "!!!!!!!!!!  named xfer successfully detected !!!!!!!!!!!"
-  echo "!!!!!!!!!! waiting for dhcp cluster recovery !!!!!!!!!!!"
+  echo "!!!!!!!!!! Named XFER Successfully Detected !!!!!!!!!!!!!!!!!!"
+  echo "?????????? Waiting for DHCP Cluster Recovery ?????????????????"
   tail -f /var/log/messages | grep --line-buffered -Ei 'Both servers have entered recover-done!' -m 1
-  echo "!!!!!!!!!!  Cluster recovery successfully detected !!!!!!!!!!"
+  echo "!!!!!!!!!! Cluster recovery successfully detected !!!!!!!!!!!!"
 fi
 
 if [ -e /var/log/syslog ]; then
-  echo "!!!!!!!!!! /var/log/syslog detected !!!!!!!!!!!!"
-  echo "!!!!!!!!!!  waiting for dns named xfer !!!!!!!!!!!"
+  echo "!!!!!!!!!! Detected /var/log/messages !!!!!!!!!!!!!!!!!!!!!!!!"
+  echo "?????????? Waiting for BIND named XFER ???????????????????????"
   tail -f /var/log/syslog | grep --line-buffered -Ei 'AXFR started' -m 2
-  echo "!!!!!!!!!!  named xfer successfully detected !!!!!!!!!!!"
-  echo "!!!!!!!!!! waiting for dhcp cluster recovery !!!!!!!!!!!"
+  echo "!!!!!!!!!! Named XFER Successfully Detected !!!!!!!!!!!!!!!!!!"
+  echo "?????????? Waiting for DHCP Cluster Recovery ?????????????????"
   tail -f /var/log/syslog | grep --line-buffered -Ei 'Both servers have entered recover-done!' -m 1
-  echo "!!!!!!!!!!  Cluster recovery successfully detected !!!!!!!!!!!!"
+  echo "!!!!!!!!!! Cluster recovery successfully detected !!!!!!!!!!!!"
 fi
 
 
