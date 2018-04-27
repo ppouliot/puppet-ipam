b13f740da882dc88089c78894ee88fa22316d4e7
Peter J. Pouliot <peter@pouliot.net>
2018-04-27 17:11:46 -0400

    attempting to update the release file some

--- release.sh
+++ release.sh
@@ -33,15 +33,14 @@ echo -n "**** PUSHING VERSION:$VERSION $BASE TO $USER/$IMAGE.git ****"
 git push
 git push --tags
 echo -n "**** DOCKER IMAGE TAGGING VERSION:$VERSION $BASE TO $USER/$IMAGE:$VERSION (centos,debian,ubuntu) ****"
-docker tag $USERNAME/$IMAGE $USERNAME/$IMAGE:version
-docker tag $USERNAME/$IMAGE-centos $USERNAME/$IMAGE-centos:$VERSION
-docker tag $USERNAME/$IMAGE-debian $USERNAME/$IMAGE-debian:$VERSION
-docker tag $USERNAME/$IMAGE-ubuntu $USERNAME/$IMAGE-ubuntu:$VERSION
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:version
+docker tag $USERNAME/$IMAGE-centos:latest $USERNAME/$IMAGE-centos:$VERSION
+docker tag $USERNAME/$IMAGE-debian:latest $USERNAME/$IMAGE-debian:$VERSION
+docker tag $USERNAME/$IMAGE-ubuntu:latest $USERNAME/$IMAGE-ubuntu:$VERSION
 
 # push it
 echo -n "**** PUSHING DOCKER IMAGE VERSION:$VERSION $BASE TO $USER/$IMAGE:$VERSION (centos,debian,ubuntu)[hub.docker.com]****"
 docker push $USERNAME/$IMAGE
-docker push $USERNAME/$IMAGE:$VERSION
-docker push $USERNAME/$IMAGE-centos:$VERSION
-docker push $USERNAME/$IMAGE-debian:$VERSION
-docker push $USERNAME/$IMAGE-ubuntu:$VERSION
+docker push $USERNAME/$IMAGE-centos
+docker push $USERNAME/$IMAGE-debian
+docker push $USERNAME/$IMAGE-ubuntu
