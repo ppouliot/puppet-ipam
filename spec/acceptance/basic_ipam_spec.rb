1b06789832b0af2e480124bb0a158db75e3968b4
Peter Pouliot <peter@pouliot.net>
2019-01-25 22:44:06 +0000

    basic acceptance tests

---
+++ spec/acceptance/basic_ipam_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper_acceptance'
+
+describe 'basic ipam' do
+  context 'default parameters' do
+    let(:pp) do
+      "
+        include ::ipam
+        # dns::server::options{'/etc/bind/named.conf.options': forwarders => ['8.8.8.8', '8.8.4.4'], }
+      "
+    end
+
+    it 'is_expected.to apply with no errors' do
+      apply_manifest(pp, catch_failures: true)
+    end
+    it 'is_expected.to be idempotent' do
+      apply_manifest(pp, catch_changes: true)
+    end
+  end
+end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,37 +1,36 @@
 require 'beaker-rspec'
+require 'beaker-puppet'
+require 'beaker/puppet_install_helper'
+require 'beaker/module_install_helper'
+require 'beaker-task_helper'
 
 UNSUPPORTED_PLATFORMS = '%w[Windows Solaris AIX]'.freeze
 
-unless ENV['RS_PROVISION'] == 'no' || ENV['BEAKER_provision'] == 'no'
-  # This will install the latest available package on el and deb based
-  # systems fail on windows and osx, and install via gem on other *nixes
-  foss_opts = { default_action: 'gem_install' }
-
-  default.is_pe? ? install_pe : install_puppet(foss_opts)
-
-  hosts.each do |host|
-    on hosts, "mkdir -p #{host['distmoduledir']}"
-  end
-end
+run_puppet_install_helper_on(hosts)
+configure_type_defaults_on(hosts)
+install_ca_certs unless pe_install?
 
 RSpec.configure do |c|
-  # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
   # Readable test descriptions
   c.formatter = :documentation
 
   # Configure all nodes in nodeset
   c.before :suite do
     hosts.each do |host|
-      # Install this module
-      copy_module_to(host, source: proj_root, module_name: '')
-      # List other dependencies here so they are installed on the host
-      on host, puppet(
-        'module',
-        'install',
-        'puppetlabs-stdlib',
-      ), acceptable_exit_codes: [0, 1]
+      # install git
+      install_package host, 'git'
+      shell('/opt/puppetlabs/puppet/bin/gem install r10k hiera-eyaml')
+      shell('curl https://raw.githubusercontent.com/ppouliot/puppet-ipam/master/Puppetfile -o /etc/puppetlabs/code/environments/production/Puppetfile')
+      shell('curl https://raw.githubusercontent.com/ppouliot/puppet-ipam/master/files/hiera/hiera.yaml -o /etc/puppetlabs/code/environments/production/hiera.yaml')
+      shell('cd /etc/puppetlabs/code/environments/production && /opt/puppetlabs/puppet/bin/r10k puppetfile install --verbose DEBUG')
+      shell('cp -R /etc/puppetlabs/code/environments/production/modules/ipam/files/hiera /etc/puppetlabs/code/environments/production/data')
+      shell('rm -rf /etc/puppetlabs/code/environments/production/modules/ipam')
+      shell("cp /etc/puppetlabs/code/environments/production/data/nodes/ipam1.contoso.ltd.yaml /etc/puppetlabs/code/environments/production/data/nodes/`facter | grep fqdn | awk '{print $3}'| sed -e 's/\"//g'| awk -F, '{print $1}'`.yaml")
+      shell("cp /etc/puppetlabs/code/environments/production/data/nodes/ipam1.contoso.ltd.yaml /etc/puppetlabs/code/environments/production/data/nodes/`facter | grep fqdn | awk '{print $3}'| sed -e 's/\"//g'| awk -F, '{print $1}'`.contoso.ltd.yaml")
+      shell("cp /etc/puppetlabs/code/environments/production/data/nodes/ipam1.contoso.ltd.yaml /etc/puppetlabs/code/environments/production/data/nodes/`facter | grep fqdn | awk '{print $3}'| sed -e 's/\"//g'`yaml")
+
     end
+    install_module_on(hosts)
+    # install_module_dependencies_on(hosts)
   end
 end
